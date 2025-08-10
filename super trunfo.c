30aed2758a1c2f54689a5a287e9412b83da881b4
Manopodbr <165099766+Manopodbr@users.noreply.github.com>
2025-08-10 02:12:07 +0000

    prpjeto pronto

--- super trunfo.c
+++ super trunfo.c
@@ -2,7 +2,7 @@
 int main(){
  
 printf("Desafio Super Trunfo!\n");
-
+printf("Vamos cadastrar a carta 1\n");
 char estado[15];
 char codigo[10];
 char nome[20];
@@ -11,16 +11,16 @@ float km;
 float pib;
 int tur;
 
-printf("Qual o estado da sua cidade: ");
-scanf("%s", &estado);
+printf("Qual estado voçê é?: ");
+scanf("%s", estado);
 printf("O estado é: %s", estado);
 
-printf("\nQual o código do seu estado: ");
-scanf("%s", &codigo);
+printf("\nQual o código da carta: ");
+scanf("%s", codigo);
 printf("O código do estado é: %s", codigo);
 
 printf("\nQual o nome da sua cidade: ");
-scanf("%s", &nome);
+scanf("%s", nome);
 printf("O nome da cidade é: %s", nome);
 
 printf("\nQual a população da sua cidade: ");
@@ -34,10 +34,58 @@ printf("A distância em km da cidade é: %.2f", km);
 printf("\nQual o PIB da sua cidade: ");
 scanf("%f", &pib);
 printf("O PIB da cidade é: %.2f", pib);
-
+while (getchar() != '\n'); // limpa o buffer do teclado
 printf("\nQuantos pontos turísticos a sua cidade tem: ");
 scanf("%d", &tur);
-printf("A cidade tem %d pontos turísticos", tur);
+printf("A quantidade de pontis turisticos é: %d", tur);
+printf("\nCadastro da carta 1 concluído!\n");
 
-printf("\n\nObrigado por participar do desafio Super Trunfo!\n");
+printf("Vamos cadastrar a carta 2\n");
+
+
+char estado2[15];
+char codigo2[10];
+char nome2[20];
+int pop2;
+float km2;
+float pib2;
+int tur2;
+
+printf("Qual estado voçê é?: ");
+scanf("%s", estado2);
+printf("O estado é: %s", estado2);       
+
+printf("\nQual o código da carta: ");
+scanf("%s", codigo2);
+printf("O código do estado é: %s", codigo2);        
+
+printf("\nQual o nome da sua cidade: ");
+scanf("%s", nome2);
+printf("O nome da cidade é: %s", nome2);
+
+printf("\nQual a população da sua cidade: ");
+scanf("%d", &pop2);
+printf("A população da cidade é: %d", pop2);
+
+printf("\nQual a distância em km da sua cidade: ");
+scanf("%f", &km2);
+printf("A distância em km da cidade é: %.2f", km2); 
+
+printf("\nQual o PIB da sua cidade: ");
+scanf("%f", &pib2);
+printf("O PIB da cidade é: %.2f", pib2);
+
+while (getchar() != '\n'); // limpa o buffer do teclado
+printf("\nQuantos pontos turísticos a sua cidade tem: ");
+scanf("%d", &tur2);
+printf("A quantidade de pontis turisticos é: %d", tur2);
+printf("\nCadastro da carta 2 concluído!\n");
+
+
+
+
+
+
+return 0;
 }
+
